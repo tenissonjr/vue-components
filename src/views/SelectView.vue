2244c142041030b9617fa7240325075e5c679c7d
Tenisson Chaves Jr. <tenisson.jr@gmail.com>
2025-02-02 18:08:19 -0300

    Adicionado exemplo de escopo de visualização por usuário

---
+++ src/views/SelectView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Select v-model="selectedOption" :options="options" optionLabel="label" placeholder="Selecione uma opção">
+      <template #value="slotProps">
+        <div v-if="slotProps.value" class="flex items-center">
+          <i :class="slotProps.value.icon" class="mr-2"></i>
+          {{ slotProps.value.label }}
+        </div>
+        <span v-else>
+          {{ slotProps.placeholder }}
+        </span>
+      </template>
+      <template #option="slotProps">
+        <div class="flex items-center">
+          <i :class="slotProps.option.icon" class="mr-2"></i>
+          {{ slotProps.option.label }}
+        </div>
+      </template>
+    </Select>
+  </div>
+  selectedOption => {{ selectedOption }}
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Select from 'primevue/select';
+
+const options = [
+  { label: 'Usuário logado', value: 'loggedUser', icon: 'pi pi-user' },
+  { label: 'Todos os usuários', value: 'allUsers', icon: 'pi pi-users' }
+];
+const selectedOption = ref<string | null>(null);
+</script>
+
+<style scoped>
+/* Adicione estilos personalizados aqui, se necessário */
+</style>
